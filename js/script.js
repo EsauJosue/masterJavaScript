8b906b47f9dde6b2c844354c359f8a1aadd258df
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2023-10-09 19:02:56 -0600

    Archivo inicial del Master

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Comentarios
+//Un comentario lo puedes integrar s tu código con //
+/* O bien puedes integrar comentarios de bloque con los siguientes
+caracteres */
+function holaMundo(){
+    console.log("Hola Mundo")
+}
+
+function holaMundoAlert(){
+    alert("Este es un hola mundo desde un alert")
+}
+
