a1ab7a9a60644c7f43439a05c977436208eae3d4
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-01-27 18:10:06 -0600

    Funciones-texto listo

---
+++ funciones-texto.js
@@ -0,0 +1,18 @@
+'use strict'
+//Transformacion de textos 
+var numero = 444;
+var texto1 = "Bienvenido al curso de JavaScript";
+var texto2 = "Es muy buen curso";
+var vacio = "";
+var arreglo = [23,45, 36, "Hola mundo"];
+
+//Convierte a texto
+console.log(typeof numero.toString())
+//Convierte a mayusculas
+console.log(texto1.toUpperCase())
+//Convierte a minusculas
+console.log(texto1.toLowerCase());
+//Calcular lobgitud de un texto
+console.log("La longitud del texto vacio es de: "+vacio.length)
+//Calcular longitud de un array
+console.log("La longitud de un array es de: ", arreglo.length)
