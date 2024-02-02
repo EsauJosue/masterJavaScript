867e1c28f6e0dd619e8aac7a4335a40190d3a878
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-02-02 13:05:33 -0600

    Clase de los arrayas con foreach callback

--- arrays.js
+++ arrays.js
@@ -1,6 +1,30 @@
 'use strict'
 var nombres = ["Victor", "Josue", "Esau"];
-console.log(typeof nombres);
-
 var lenguajes = new Array("Python", "JavaScript", "Kotlin");
-console.log(lenguajes[1]);
+
+var eleccion = parseInt(prompt("Que elemento quieres mostrar del arregolo: ",0))
+if (eleccion >= nombres.length){
+    alert("el arreglo es menor al numero que estas indicando")
+}else{
+    console.log(nombres[eleccion]);
+
+}
+
+//Usando for
+document.write("<h1> Lenguajes de programación </h1>");
+document.write("<ul>");
+for(var i = 0; i < lenguajes.length; i ++){
+    document.write("<li>"+ lenguajes[i] + "</li>");
+}
+document.write("</ul>");
+
+//Usando Foreach
+
+document.write("<h2> Lenguajes de programación usando ForEach</h2>");
+document.write("<ul>");
+lenguajes.forEach((lenguajes)=>{ // Los parametros que naturalmente recibe esta función de callback son (elemento, indice, arreglo)
+    document.write("<li>"+ lenguajes + "</li>");
+});
+document.write("</ul>");
+
+
